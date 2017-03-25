19e35f6dde902fc92c70128f7bf0c868d2970ad9
pintoXD <calangodeguerra@outlook.com>
2017-03-25 11:14:07 -0300

    layout admin_dash desabilitado. Rota aponta para admin_dash/index agora. Funfando

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
       passwords: "crew/admins/passwords",
       confirmations: 'crew/admins/confirmations'
     },
-      path: '/',
+      path: "/",
     path_names: {
       sign_in: 'login',
       sign_out: 'logout',
@@ -28,69 +28,27 @@ Rails.application.routes.draw do
     }
 
     devise_scope :admin do
-      resources :admin
+
       authenticated  do
         root 'admin_dashboard#index',  as: :authenticated_admin_root
-
-
-        # resources :users
-        # #resources :lots
-        # resources :admins
-
-
-
-
-
-        # get '/qualified_users' => 'users#qualified', as: :users_qualified
-        # get '/disqualified_users' => 'users#disqualified', as: :users_disqualified
-        # get '/waiting_list' => 'users#waiting_list', as: :users_waiting_list
-        # get '/pays_list' => 'users#pays_list', as: :users_pays_list
-        #
-        #
-        # #USUARIO ADMIN
-        # patch 'move_user_to_lot/:user_id/:lot_id' => 'admins_methods#move_user_to_lot', as: :move_user_to_lot
-        # patch 'change_users/:user_id/:user_2_email' => 'admins_methods#change_users', as: :change_users_position
-        # patch 'disqualify/:id' => 'admins_methods#disqualify_user', as: :disqualify_user
-        # patch 'move_first_user_to_lot/:lot_id' => 'admins_methods#move_first_user_to_lot', as: :move_first_user_to_lot
-        #
-        # #PAYMENTS
-        # patch 'change_payment_status/:id/:status' => 'admins_methods#change_payment_status', as: :change_payment_status
-        # patch 'set_billet_portion_paid/:id/:portion_paid' => 'admins_methods#billet_portion_paid', as: :set_billet_portion_paid
-        # patch 'remove_payment_method/:id' => 'admins_methods#remove_payment_method', as: :change_payment_method
-        #
-        # #sistema
-        # get '/info_system' => 'admins#info', as: :system_info
-        # get '/ejs_list' => 'users#ejs_list', as: :ej_list
-        #
-        # #excel
-        # get 'excel/lot/users/:id' => 'excel#lot_users', as: :download_lot_users_excel
-        # #relatorio
-        # get 'relatorio' => 'excel#excel_handler', as: :excel_handler
-        # get 'excel/generate_xls' => 'excel#generate_xls', as: :generate_xls, format: :xls
-        # #ej
-        # get 'excel/ejlist' => 'excel#ejlist', format: :xls_ejlist
-        #
-        # #acess account
-        # patch 'login_user/:user_id' => 'admins_methods#login', as: :login_user
       end
 
       unauthenticated do
         #root to: "admins/sessions#new", as: :unauthenticated_admin_root
 
       end
+
+      #get "logout" => 'admins/sessions#destroy'
+
+
     end
 
 
 
+        resources :admin
+
 
   end
-
-
-
-
-
-
-
 
 
   devise_for :users,
@@ -136,13 +94,13 @@ Rails.application.routes.draw do
 
 
   resource :user
-
-  devise_scope :admin do
-          get '/login_admin' => 'devise/sessions#new'
-          get '/logout_admin' => 'devise/sessions#destroy'
-  end
-
- resource :admin, :controller => "admin"
+ #
+ #  devise_scope :admin do
+ #          get '/login_admin' => 'devise/sessions#new'
+ #          get '/logout_admin' => 'devise/sessions#destroy'
+ #  end
+ #
+ # resource :admin, :controller => "admin"
 
 
 
